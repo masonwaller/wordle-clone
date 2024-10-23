5675f0ee75f477e7d827472fa4d08cc3ac628c17
Mason Waller <masonwaller805@gmail.com>
2024-10-23 11:48:11 -0600

    fix add letter event listener and add enter case

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import "./App.css";
 import MainGrid from "./components/main-grid/MainGrid.tsx";
 
 function App() {
+  const [currentRow, setCurrentRow] = useState(1);
+
   const [rowOne, setRowOne] = useState([]);
   const [rowTwo, setRowTwo] = useState([]);
   const [rowThree, setRowThree] = useState([]);
@@ -12,11 +14,54 @@ function App() {
   const [rowSix, setRowSix] = useState([]);
 
   const keyPressEvent = (event) => {
-    console.log(rowOne);
     // Get the pressed key
     const key = event.key;
-    if (rowOne.length < 5) {
-      setRowOne([...rowOne, key]);
+
+    let rowToEdit;
+    let rowToEditFunction;
+    switch (currentRow) {
+      case 1:
+        rowToEdit = rowOne;
+        rowToEditFunction = setRowOne;
+        break;
+      case 2:
+        rowToEdit = rowTwo;
+        rowToEditFunction = setRowTwo;
+        break;
+      case 3:
+        rowToEdit = rowThree;
+        rowToEditFunction = setRowThree;
+        break;
+      case 4:
+        rowToEdit = rowFour;
+        rowToEditFunction = setRowFour;
+        break;
+      case 5:
+        rowToEdit = rowFive;
+        rowToEditFunction = setRowFive;
+        break;
+      case 6:
+        rowToEdit = rowSix;
+        rowToEditFunction = setRowSix;
+        break;
+      default:
+        break;
+    }
+
+    switch (key) {
+      case "Enter":
+        // Check if the current row is 6
+        if (rowToEdit.length === 5) {
+          setCurrentRow(currentRow + 1);
+        } else {
+          console.log("Row is not full");
+        }
+        break;
+      default:
+        if (rowToEdit.length < 5) {
+          rowToEditFunction([...rowToEdit, key]);
+        }
+        break;
     }
   };
 
