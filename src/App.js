22ffa930232f111d9b4d2338ab879ef2054b8da9
Mason Waller <masonwaller805@gmail.com>
2024-11-08 12:41:38 -0700

    removed a bunch of buttons from keyboard

--- src/App.js
+++ src/App.js
@@ -178,7 +178,63 @@ function App() {
         row6={row6}
       />
       <div style={{ width: "70%" }}>
-        <Keyboard onKeyPress={keyPressEvent} />
+        <Keyboard
+          onKeyPress={keyPressEvent}
+          excludeFromLayout={{
+            default: [
+              "@",
+              ".com",
+              "1",
+              "2",
+              "3",
+              "4",
+              "5",
+              "6",
+              "7",
+              "8",
+              "9",
+              "0",
+              "`",
+              "-",
+              `=`,
+              "space",
+              "[",
+              "]",
+              "\\",
+              ";",
+              "'",
+              ",",
+              ".",
+              "/",
+            ],
+            shift: [
+              "@",
+              ".com",
+              "1",
+              "2",
+              "3",
+              "4",
+              "5",
+              "6",
+              "7",
+              "8",
+              "9",
+              "0",
+              "`",
+              "-",
+              `=`,
+              "space",
+              "[",
+              "]",
+              "\\",
+              ";",
+              "'",
+              ",",
+              ".",
+              "/",
+            ],
+          }}
+        />
       </div>
     </div>
   );
