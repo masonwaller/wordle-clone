6000e987d7ddee5dc241e96f70f510c41c83ffd1
Mason Waller <masonwaller805@gmail.com>
2025-01-23 13:00:51 -0700

    refactor style attribute to be a class in ad box

--- src/components/ad-box/AdBox.tsx
+++ src/components/ad-box/AdBox.tsx
@@ -4,13 +4,7 @@ import "./AdBox.css";
 export default class AdBox extends React.Component {
   render() {
     return (
-      <div
-        style={{
-          width: "150px",
-          justifySelf: "flex-start",
-          alignSelf: "center",
-        }}
-      >
+      <div className="ad-box">
         <img
           src="https://digitalsynopsis.com/wp-content/uploads/2023/12/creative-ads-coke-long-can.jpg"
           alt="ad"
